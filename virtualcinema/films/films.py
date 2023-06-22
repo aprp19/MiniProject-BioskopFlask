c273b948f4f6b3dc720ffda3206d19d64d005a2d
Angga Puja Restu Prakasa <kierska19@gmail.com>
2023-06-22 15:51:57 +0700

    Added Get and Post Film

--- virtualcinema/films/films.py
+++ virtualcinema/films/films.py
@@ -1,18 +1,71 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from virtualcinema.auth import auth
-from virtualcinema.model.models import ModelAccount
+from virtualcinema.db import db_session
+from virtualcinema.model.models import ModelAccount, ModelFilm, ModelFilmCategory, ModelFilmSchedule, ModelCategory
 
 film = Blueprint('film', __name__)
 
 
-@film.route('/film')
-@auth
-def handler_get_account():
-    return 'Hello World!'
+@film.route('/film/<id_film>', methods=['GET'])
+def handler_get_film(id_film):
+    if id_film == 'all':
+        query = ModelFilm.query.all()
+        response = [{
+            'id_film': row.id_film,
+            'film_name': row.film_name,
+            'film_duration': row.film_duration,
+            'category': ['Not yet available' if ModelCategory.query.filter(ModelFilmCategory.id_film == row.id_film,
+                                                                           ModelCategory.id_category == ModelFilmCategory.id_film).first() is None else cat.category_name
+                         for cat in ModelCategory.query.filter(ModelFilmCategory.id_film == row.id_film,
+                                                               ModelCategory.id_category == ModelFilmCategory.id_film).all()],
+            'film_price': row.film_price,
+            'film_selling': row.film_selling,
+        } for row in query]
+
+        return {"Message": "Success", "Count": len(response), "Data": response}, 200
+
+    query = ModelFilm.query.filter_by(id_film=id_film).first()
+    if not query:
+        return {"Error": "Film not found"}, 404
+    else:
+        response = {
+            'id_film': query.id_film,
+            'film_name': query.film_name,
+            'film_duration': query.film_duration,
+            'category': ['Not yet available' if ModelCategory.query.filter(ModelFilmCategory.id_film == query.id_film,
+                                                                           ModelCategory.id_category == ModelFilmCategory.id_film).first() is None else cat.category_name
+                         for cat in ModelCategory.query.filter(ModelFilmCategory.id_film == query.id_film,
+                                                               ModelCategory.id_category == ModelFilmCategory.id_film).all()],
+            'film_price': query.film_price,
+            'film_selling': query.film_selling,
+        }
+        return {"Message": "Success", "Data": response}, 200
 
 
-@film.route('/film')
+@film.route('/film', methods=['POST'])
 @auth
-def handler_post_account():
-    return 'Hello World!'
+def handler_post_film():
+    session = ModelAccount.query.filter_by(u_username=request.authorization.username).first()
+    if session.u_role == 'Admin':
+        if request.is_json:
+            json = request.get_json()
+            add_film = ModelFilm(
+                film_name=json['film_name'],
+                film_duration=json['film_duration'],
+                film_price=json['film_price']
+            )
+            if ModelFilm.query.filter_by(film_name=add_film.film_name).first():
+                return {"Error": "Film already exists"}, 400
+            db_session.add(add_film)
+            db_session.commit()
+            return {"Message": "Film added succesfully", "Data": f"{add_film.film_name}"}, 200
+        else:
+            return {"Message": "Invalid Request"}, 400
+    else:
+        return {"Message": "Unauthorized"}, 403
+
+
+# @film.route('/film_category/<id_film>', methods=['POST'])
+# @auth
+# def
--- virtualcinema/model/models.py
+++ virtualcinema/model/models.py
@@ -26,7 +26,8 @@ class ModelFilm(db.Model):
     id_film = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     film_name = db.Column(db.String(25), nullable=False)
     film_duration = db.Column(db.Integer, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+    film_price = db.Column(db.Integer, nullable=False)
+    film_selling = db.Column(db.Integer, nullable=False)
     film_category = db.relationship('ModelFilmCategory', backref='film', lazy=True)
 
 
@@ -39,14 +40,14 @@ class ModelCategory(db.Model):
 
 
 class ModelFilmCategory(db.Model):
-    __tablename__ = 'film_category'
+    __tablename__ = 'filmcategory'
 
     id_film = db.Column(db.Integer, db.ForeignKey('film.id_film'), primary_key=True)
     id_category = db.Column(db.Integer, db.ForeignKey('category.id_category'), primary_key=True)
 
 
 class ModelFilmSchedule(db.Model):
-    __tablename__ = 'schedule'
+    __tablename__ = 'filmschedule'
 
     id_schedule = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     id_film = db.Column(db.Integer, db.ForeignKey('film.id_film'), nullable=False)
@@ -54,7 +55,7 @@ class ModelFilmSchedule(db.Model):
     schedule_date = db.Column(db.String(25), nullable=False)
     schedule_time = db.Column(db.String(25), nullable=False)
     schedule_price = db.Column(db.Integer, nullable=False)
-    order = db.relationship('ModelOrder', backref='schedule', lazy=True)
+    # film_order = db.relationship('ModelOrder', backref='schedule', lazy=True)
 
 
 class ModelSeat(db.Model):
@@ -66,7 +67,7 @@ class ModelSeat(db.Model):
 
 
 class ModelOrder(db.Model):
-    __tablename__ = 'order'
+    __tablename__ = 'orders'
 
     id_order = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     id_user = db.Column(db.Integer, db.ForeignKey('account.id_user'), nullable=False)
@@ -75,7 +76,7 @@ class ModelOrder(db.Model):
     order_date = db.Column(db.String(25), nullable=False)
     order_qty = db.Column(db.Integer, nullable=False)
     order_total = db.Column(db.Integer, nullable=False)
-    orderseat = db.relationship('ModelOrderSeat', backref='order', lazy=True)
+    # orderseat = db.relationship('ModelOrderSeat', backref='order', lazy=True)
 
 
 class ModelOrderSeat(db.Model):
@@ -86,7 +87,7 @@ class ModelOrderSeat(db.Model):
 
 
 class ModelPayment(db.Model):
-    __tablename__ = 'payment'
+    __tablename__ = 'payments'
 
     id_payment = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     id_order = db.Column(db.Integer, db.ForeignKey('order.id_order'), nullable=False)
