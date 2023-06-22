c0e4a9e5c3f6ea7f3ef2c4732bbddcd87797217a
Angga Puja Restu Prakasa <kierska19@gmail.com>
2023-06-22 17:01:10 +0700

    Fix Models relations, adding film category end apis

--- virtualcinema/films/films.py
+++ virtualcinema/films/films.py
@@ -66,6 +66,117 @@ def handler_post_film():
         return {"Message": "Unauthorized"}, 403
 
 
-# @film.route('/film_category/<id_film>', methods=['POST'])
-# @auth
-# def
+@film.route('/film/<id_film>', methods=['PUT'])
+@auth
+def handler_put_film(id_film):
+    session = ModelAccount.query.filter_by(u_username=request.authorization.username).first()
+    if session.u_role == 'Admin':
+        if request.is_json:
+            json = request.get_json()
+            update_film = ModelFilm.query.filter_by(id_film=id_film).first()
+            if not update_film:
+                return {"Error": "Film not found"}, 404
+            else:
+                update_film.film_name = json['film_name']
+                update_film.film_duration = json['film_duration']
+                update_film.film_price = json['film_price']
+                db_session.add(update_film)
+                db_session.commit()
+                return {"Message": "Film updated succesfully", "Data": f"{update_film.film_name}"}, 200
+        else:
+            return {"Message": "Invalid Request"}, 400
+    else:
+        return {"Message": "Unauthorized"}, 403
+
+
+@film.route('/film/<id_film>', methods=['DELETE'])
+@auth
+def handler_delete_film(id_film):
+    session = ModelAccount.query.filter_by(u_username=request.authorization.username).first()
+    if session.u_role == 'Admin':
+        query = ModelFilm.query.filter_by(id_film=id_film).first()
+        if not query:
+            return {"Error": "Film not found"}, 404
+        db_session.delete(query)
+        db_session.commit()
+        return {"Message": "Film deleted succesfully"}, 200
+    else:
+        return {"Message": "Unauthorized"}, 403
+
+
+@film.route('/category/<id_category>', methods=['GET'])
+def handler_get_category(id_category):
+    if id_category == 'all':
+        query = ModelCategory.query.all()
+        response = [{
+            'id_category': row.id_category,
+            'category_name': row.category_name,
+        } for row in query]
+        return {"Message": "Success", "Count": len(response), "Data": response}, 200
+    query = ModelCategory.query.filter_by(id_category=id_category).first()
+    if not query:
+        return {"Error": "Category not found"}, 404
+    else:
+        response = {
+            'id_category': query.id_category,
+            'category_name': query.category_name,
+        }
+        return {"Message": "Success", "Data": response}, 200
+
+
+@film.route('/category', methods=['POST'])
+@auth
+def handler_post_category():
+    session = ModelAccount.query.filter_by(u_username=request.authorization.username).first()
+    if session.u_role == 'Admin':
+        if request.is_json:
+            json = request.get_json()
+            add_category = ModelCategory(
+                category_name=json['category_name']
+            )
+            if ModelCategory.query.filter_by(category_name=add_category.category_name).first():
+                return {"Error": "Category already exists"}, 400
+            db_session.add(add_category)
+            db_session.commit()
+            return {"Message": "Category added succesfully", "Data": f"{add_category.category_name}"}, 200
+        else:
+            return {"Message": "Invalid Request"}, 400
+    else:
+        return {"Message": "Unauthorized"}, 403
+
+
+@film.route('/category/<id_category>', methods=['DELETE'])
+@auth
+def handler_delete_category(id_category):
+    session = ModelAccount.query.filter_by(u_username=request.authorization.username).first()
+    if session.u_role == 'Admin':
+        query = ModelCategory.query.filter_by(id_category=id_category).first()
+        if not query:
+            return {"Error": "Category not found"}, 404
+        db_session.delete(query)
+        db_session.commit()
+        return {"Message": "Category deleted succesfully"}, 200
+    else:
+        return {"Message": "Unauthorized"}, 403
+
+
+@film.route('/film_category/<id_film>', methods=['POST'])
+@auth
+def handler_post_film_category(id_film):
+    session = ModelAccount.query.filter_by(u_username=request.authorization.username).first()
+    if session.u_role == 'Admin':
+        if request.is_json:
+            json = request.get_json()
+            add_film_category = ModelFilmCategory(
+                id_film=id_film,
+                id_category=json['id_category']
+            )
+            if ModelFilmCategory.query.filter_by(id_film=id_film, id_category=json['id_category']).first():
+                return {"Error": "Film category already exists"}, 400
+            db_session.add(add_film_category)
+            db_session.commit()
+            return {"Message": "Film category added succesfully", "Data": f"{add_film_category.id_category}"}, 200
+        else:
+            return {"Message": "Invalid Request"}, 400
+    else:
+        return {"Message": "Unauthorized"}, 403
--- virtualcinema/model/models.py
+++ virtualcinema/model/models.py
@@ -10,6 +10,7 @@ class ModelAccount(db.Model):
     u_password = db.Column(db.String(25), nullable=False)
     u_role = db.Column(db.String(25), nullable=False)
     wallet = db.relationship('ModelWallet', backref='account', lazy=True)
+    order = db.relationship('ModelOrder', backref='account', lazy=True)
 
 
 class ModelWallet(db.Model):
@@ -29,6 +30,7 @@ class ModelFilm(db.Model):
     film_price = db.Column(db.Integer, nullable=False)
     film_selling = db.Column(db.Integer, nullable=False)
     film_category = db.relationship('ModelFilmCategory', backref='film', lazy=True)
+    film_schedule = db.relationship('ModelFilmSchedule', backref='film', lazy=True)
 
 
 class ModelCategory(db.Model):
@@ -51,11 +53,10 @@ class ModelFilmSchedule(db.Model):
 
     id_schedule = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     id_film = db.Column(db.Integer, db.ForeignKey('film.id_film'), nullable=False)
-    id_user = db.Column(db.Integer, db.ForeignKey('account.id_user'), nullable=False)
     schedule_date = db.Column(db.String(25), nullable=False)
     schedule_time = db.Column(db.String(25), nullable=False)
     schedule_price = db.Column(db.Integer, nullable=False)
-    # film_order = db.relationship('ModelOrder', backref='schedule', lazy=True)
+    film_order = db.relationship('ModelOrder', backref='schedule', lazy=True)
 
 
 class ModelSeat(db.Model):
@@ -71,18 +72,18 @@ class ModelOrder(db.Model):
 
     id_order = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     id_user = db.Column(db.Integer, db.ForeignKey('account.id_user'), nullable=False)
-    id_schedule = db.Column(db.Integer, db.ForeignKey('schedule.id_schedule'), nullable=False)
+    id_schedule = db.Column(db.Integer, db.ForeignKey('filmschedule.id_schedule'), nullable=False)
     order_seat = db.Column(db.String(25), nullable=False)
     order_date = db.Column(db.String(25), nullable=False)
     order_qty = db.Column(db.Integer, nullable=False)
     order_total = db.Column(db.Integer, nullable=False)
-    # orderseat = db.relationship('ModelOrderSeat', backref='order', lazy=True)
+    orderseat = db.relationship('ModelOrderSeat', backref='order', lazy=True)
 
 
 class ModelOrderSeat(db.Model):
     __tablename__ = 'orderseat'
 
-    id_order = db.Column(db.Integer, db.ForeignKey('order.id_order'), primary_key=True)
+    id_order = db.Column(db.Integer, db.ForeignKey('orders.id_order'), primary_key=True)
     id_seat = db.Column(db.Integer, db.ForeignKey('seat.id_seat'), primary_key=True)
 
 
